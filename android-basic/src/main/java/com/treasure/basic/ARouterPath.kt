d74f8deb688785a520270a1e33274e3847948a1c
王春涛 chuntao.wang <treasure.ct6@gmail.com>
2026-09-15 13:55:29 +0800

    Delete android-basic directory

--- android-basic/src/main/java/com/treasure/basic/ARouterPath.kt
+++
@@ -1,7 +0,0 @@
-package com.treasure.basic
-
-object ARouterPath {
-    const val ACTIVITY_MAIN = "/activity/main"
-    const val ACTIVITY_LOGIN = "/login"
-    const val ACTIVITY_PRIVACY = "/privacy"
-}
--- android-basic/src/main/java/com/treasure/basic/helper/CommonCallback.java
+++
@@ -1,8 +0,0 @@
-package com.treasure.basic.helper;
-
-public interface CommonCallback<T> {
-    void onContinue(T value);
-
-    default void onError(String value) {
-    }
-}
